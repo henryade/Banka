936fa8faed59ff76fcddb88b56ef31cde1a0d76f
Adeola Adeyemo <adeoladeyemo@gmail.com>
2019-04-13 00:07:23 +0100

    ch(deploy): Deploy app deployment to heroku [Finishes #165325093]

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 import app from "./app";
 // eslint-disable-next-line import/named
-import { port } from "./config";
+import { Port } from "./config";
 
+const port = process.env.PORT || Port;
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`Server running at port:${port}...`);
