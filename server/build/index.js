b7f535ceb8ac83026dd3915de6f54009b6fc8719
henryade <47576037+henryade@users.noreply.github.com>
2019-04-13 00:08:34 +0100

    ch(deploy): Deploy app (#61)
    deployment to heroku
    [Finishes #165325093]

--- server/build/index.js
+++ server/build/index.js
@@ -6,8 +6,9 @@ var _config = require("./config");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
+var port = process.env.PORT || _config.port;
 // eslint-disable-next-line import/named
-_app["default"].listen(_config.port, function () {
+_app["default"].listen(port, function () {
   // eslint-disable-next-line no-console
-  console.log("Server running at port:".concat(_config.port, "..."));
+  console.log("Server running at port:".concat(port, "..."));
 });
